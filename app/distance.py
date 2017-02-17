14e61b62bc0cfd3107187b3dcba723f0dc55bead
justinw238 <jlw15@msstate.edu>
2017-02-17 12:39:21 -0600

    main

--- app/distance.py
+++ app/distance.py
@@ -2,19 +2,23 @@ import math
 
 class Distance():
 	def __init__(self):
-		self.x1 = 0
-		self.x2 = 0
-		self.y1 = 0
-		self.y2 = 0
+		#self.x1 = 0
+		#self.x2 = 0
+		#self.y1 = 0
+		#self.y2 = 0
+		pass
 	
-	def distance(self, x1, x2, y1, y2):
+	def distance(self, x1, y1, x2, y2):
+		
 		try:
-			self.x1 = float(x1)
-			self.x2 = float(x2)
-			self.y1 = float(y1)
-			self.y2 = float(y2)
+			x1 = float(x1)
+			x2 = float(x2)
+			y1 = float(y1)
+			y2 = float(y2)
 		except ValueError:
 			raise ValueError("Invalid Input")
+		#result = math.sqrt((self.x2 - self.x1)**2 + (self.y2 - self.y1)**2)
 		
-		return math.sqrt(self.x1 * self.x2 + self.y1 * self.y2)
+		result = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+		return result
 		
--- qa_tdd.py
+++ qa_tdd.py
@@ -3,11 +3,11 @@ from app.distance import Distance
 def doDistance(choice):
 	dist = Distance()
 	x1 = input("Enter point X1: ")
-	x2 = input("Enter point X2: ")
 	y1 = input("Enter point Y1: ")
+	x2 = input("Enter point X2: ")
 	y2 = input("Enter point Y2: ")
 	
-	result = dist.distance(x1, x2, y1, y2)
+	result = dist.distance(x1, y1, x2, y2)
 
 	print("Distance is: ", result)	
 	print()
@@ -15,7 +15,7 @@ def doDistance(choice):
 	
 def main():
 	choice = -1
-	choice_list = ['0','1','2','3','4','q', 'x']
+	choice_list = ['0','1','2','3','4']
 	while choice != 0:
 		while choice not in choice_list:
 			print("1: Calculate Body Mass Index")
@@ -29,6 +29,8 @@ def main():
 		
 		if choice == '4':
 			result = doDistance(choice)
+		if choice == '0':
+			return
 			
 		choice = -1
 if __name__ == '__main__':
