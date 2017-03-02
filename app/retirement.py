d32d6e93a9f917d9d32dfbdde04d1304f46b95c6
Devin-Neal <dbn38@msstate.edu>
2017-03-02 15:48:54 -0600

    Add files via upload

---
+++ app/retirement.py
@@ -0,0 +1,39 @@
+class Retirement():
+
+	#age = input("Enter your age: ")
+	#salary = input("Enter your annual salary: ")
+	#saving = input("Enter your percentage saved: ")
+	#goal = input("Enter your retirement savings goal: ")
+
+	def retirement(self, age, salary, saving, goal):
+
+		try:
+			age = int(age)
+			salary = int(salary)
+			saving = int(saving)
+			goal = int(goal)
+
+		except ValueError:
+			raise ValueError("Invalid Input")
+
+		if (age < 0 or age >= 100):
+			raise ValueError("Invalid Age")
+
+		if (salary < 0):
+			raise ValueError("Invalid Salary")
+
+		if (saving < 0 or saving > 100):
+			raise ValueError("Invalid Saving")
+
+		if (saving < 0):
+			raise ValueError("Invalid Goal")
+
+		total = 0
+		while age < 100:
+			total += (salary * (saving / 100))
+			if total >= goal:
+				print(age)
+				return age
+			age+=1
+
+		return 101
