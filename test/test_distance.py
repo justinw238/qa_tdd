548dec5460c6954519a9f820e49d9ad7e6f49014
justinw238 <jlw15@msstate.edu>
2017-03-02 12:03:13 -0600

    added tests

--- test/test_distance.py
+++ test/test_distance.py
@@ -5,11 +5,17 @@ class TDD(unittest.TestCase):
 	def setUp(self):
 		self.dist = Distance()
 		
-	def test_distance_returns_correct_results(self):
+	def test_distance_returns_correct_results_positive_input(self):
 		result = self.dist.distance(1,1,1,2)
 		self.assertEqual(1, result)
+		
+	def test_distance_returns_correct_results_negative_input(self):
 		result = self.dist.distance(-1,-1, -1, -2)
 		self.assertEqual(1, result)
+		
+	def test_distance_returns_correct_results_mixed_input(self):
+		result = self.dist.distance(-1,1, 1, 1)
+		self.assertEqual(2, result)		
 		
 	def test_distance_only_int_input(self):
 		self.assertRaises(ValueError, self.dist.distance, 'one', 'one', 'one', 'one')
