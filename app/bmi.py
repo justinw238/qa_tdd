cd84db5d0ed0907f583e9dfafe95a2acd683858e
DarrianBlack <cwm186@msstate.edu>
2017-03-02 17:43:10 -0600

    Added negative value exception

--- app/bmi.py
+++ app/bmi.py
@@ -5,7 +5,13 @@ class BMI:
             weight = float(weight)
             height = float(height)
         except ValueError:
-            raise(ValueError("Invalid Input"))
+            print("Input was not a float.")
+            raise TypeError
+
+        if(weight < 0 or height < 0):
+            print("Input(s) were values less than 0.")
+            raise ValueError
+
 
         weight *= 0.45
         height *= 0.025
